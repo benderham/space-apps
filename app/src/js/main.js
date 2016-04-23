7d7257929ac806ca769e1d11ddb83de2c63ce359
Ben Derham <ben@benderham.com>
2016-04-23 16:29:16 +0800

    charts js

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -24,24 +24,18 @@ $(document).ready(function () {
     labels: ["January", "February", "March", "April", "May", "June", "July"],
     datasets: [
         {
-            label: "My First dataset",
+            label: "2015",
+            fill: false,
             backgroundColor: "#2196F3",
-            strokeColor: "rgba(220,220,220,1)",
-            pointColor: "rgba(220,220,220,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(220,220,220,1)",
+            borderColor: "#2196F3",
             data: [65, 59, 80, 81, 56, 55, 40]
         },
         {
-            label: "My Second dataset",
-            backgroundColor: "#BBDEFB",
-            strokeColor: "#BBDEFB",
-            pointColor: "#BBDEFB",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(151,187,205,1)",
-            data: [28, 48, 40, 19, 86, 27, 90]
+            label: "2016",
+            fill: false,
+            backgroundColor: "#4CAF50",
+            borderColor: "#4CAF50",
+            data: [28, 48, 40, 19]
         }
     ]
   };
